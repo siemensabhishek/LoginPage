37a5133dad50b133e03dc7f6fb21f80f79ec9acf
Singh <abhishek.singh@siemens-healthineers.com>
2023-03-24 10:29:05 +0530

    hh

--- LoginPage/ViewModel/LoginViewModel.cs
+++ LoginPage/ViewModel/LoginViewModel.cs
@@ -1,25 +1,13 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Security;
-using System.Security.Cryptography.X509Certificates;
-using System.Security.Policy;
-using System.Security.Principal;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Input;
-using LoginPage.Model;
-using LoginPage.ViewModel;
+﻿using LoginPage.View;
 using Newtonsoft.Json;
-using LoginPage.View;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace LoginPage.ViewModel
 {
-    public class LoginViewModel: ViewModelBase
+    public class LoginViewModel : ViewModelBase
     {
         // fields
         private int _username;
@@ -35,7 +23,7 @@ namespace LoginPage.ViewModel
         public int Password
         {
             get { return _password; }
-            set{ _password = value; OnPropertyChanged("Password"); }
+            set { _password = value; OnPropertyChanged("Password"); }
         }
         public string ErrorMessage
         {
@@ -91,7 +79,7 @@ namespace LoginPage.ViewModel
         public LoginViewModel()
         {
             LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
-            RecoverPasswordCommand = new ViewModelCommand(p => ExecuteRecoverPassword("",""));
+            RecoverPasswordCommand = new ViewModelCommand(p => ExecuteRecoverPassword("", ""));
 
         }
 
@@ -103,13 +91,51 @@ namespace LoginPage.ViewModel
 
 
         // -----------------
-/*
+        /*
+                public void ExecuteLoginCommand(object obj)
+                {
+
+                    var isValidInput = Int32.TryParse(Username,out int custId);
+                    var temp = IsValidCustomer(custId);
+                    if (!IsValidCustomer(custId).Result|| !isValidInput)
+                    {
+                        //throw some error;
+                        ErrorMessage = "InValid UserName or Password";
+
+                    }
+                    else
+                    {
+                        //Proceed to login
+
+                        //Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(Username), null);
+                        //IsViewVisible = false;
+
+                        var Page2 = new InfoView(); //create your new form.
+                        Page2.Show(); //show the new form.
+                         //only if you want to close the current form.
+
+
+                    }
+                    // LoadAllCustomer();
+
+
+
+                    //bool ValidLogin(string fName, string lName)
+                    //{
+                    //    if (Customer.firstName ! = )
+                    //        return false;
+                    //}
+
+
+                }
+        */
+
         public void ExecuteLoginCommand(object obj)
         {
 
-            var isValidInput = Int32.TryParse(Username,out int custId);
-            var temp = IsValidCustomer(custId);
-            if (!IsValidCustomer(custId).Result|| !isValidInput)
+            // var isValidInput = Int32.TryParse(Username, out int custId);
+            var temp = IsValidCustomer(Username, Password);
+            if (!temp.Result)
             {
                 //throw some error;
                 ErrorMessage = "InValid UserName or Password";
@@ -119,48 +145,10 @@ namespace LoginPage.ViewModel
             {
                 //Proceed to login
 
-                //Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(Username), null);
-                //IsViewVisible = false;
-                
-                var Page2 = new InfoView(); //create your new form.
-                Page2.Show(); //show the new form.
-                 //only if you want to close the current form.
-
-
-            }
-            // LoadAllCustomer();
-
-
-
-            //bool ValidLogin(string fName, string lName)
-            //{
-            //    if (Customer.firstName ! = )
-            //        return false;
-            //}
-
-
-        }
-*/
-
-        public void ExecuteLoginCommand(object obj)
-        {
-
-           // var isValidInput = Int32.TryParse(Username, out int custId);
-           var temp = IsValidCustomer(Username, Password);
-            if (temp.Result.ToString() =="yes")
-            {
-                //throw some error;
-                ErrorMessage = "InValid UserName or Password";
-
-            }
-            else
-            {
-                //Proceed to login
-
 
                 var Page2 = new InfoView(); //create your new form.
                 Page2.Show(); //show the new form.
-                              //only if you want to close the current form.
+
 
             }
         }
@@ -169,53 +157,101 @@ namespace LoginPage.ViewModel
 
 
 
+        /*
 
-
-        //async Task<bool> IsValidCustomer(int custId)
-        //{
-        //    //  List<Customer> customers = new List<Customer>();
-        //    string url = $"https://localhost:7172/customer/validCustomer/{custId}";
-        //    // make an API request 
-        //    using (HttpClient client = new HttpClient())
-        //    {
-
-        //        var response = await Task.Run(() => client.GetAsync(url).Result);
-        //        //  string json = await response.Content.ReadAsStringAsync();
-        //        // deserilizin the json
-        //        // customers = JsonConvert.DeserializeObject<List<Customer>>(json);
-        //        if (response != null) return true;
-
-        //    }
-        //    return false;
-
-        //}
-
-
-
-        async Task<string> IsValidCustomer(int custId, int password)
+        async Task<bool> IsValidCustomer(int custId, int password)
         {
-            string url = $"https://localhost:7172/customer/validCustomer/{custId}/{password}";
+            //  List<Customer> customers = new List<Customer>();
+            string url = $"https://localhost:7172/customer/validCustomer/{custId}";
             // make an API request 
-           // var response = "no";
             using (HttpClient client = new HttpClient())
             {
-              // var response = client.GetAsync(url);
-             //   response.Wait();
-                //  var temp = JsonConvert.DeserializeObject(response);
-                var response = await Task.Run(() => client.GetAsync(url).Result);
-                string temp = await response.Content.ReadAsStringAsync();
-                // deserilizin the json
-              //  var customers = JsonConvert.DeserializeObject(response);
-                if (temp != "no") return "yes";
 
+                var response = await Task.Run(() => client.GetAsync(url).Result);
+                //  string json = await response.Content.ReadAsStringAsync();
+                // deserilizin the json
+                // customers = JsonConvert.DeserializeObject<List<Customer>>(json);
+                if (response != null) return true;
 
             }
-            return "no";
+            return false;
+
+        }
+        */
+
+
+
+        async Task<bool> IsValidCustomer(int CustId, int password)
+        {
+            //string url = $"https://localhost:7172/customer/customer_details";
+            //using(HttpClient client = new HttpClient())
+            //{
+
+            //    var response = await client.GetAsync(url);
+
+
+            //   // var response = client.GetAsync(url);
+            //    //  response.Wait();
+            // //   if (response != "no") return "yes";
+
+            //}
+            //return "no";
+
+
+
+            var url = $"https://localhost:7172/customer/validCustomer/{CustId}/{password}";
+
+            using (var client = new HttpClient())
+            {
+
+                var msg = new HttpRequestMessage(HttpMethod.Get, url);
+                msg.Headers.Add("User-Agent", "C# Program");
+                var res = client.SendAsync(msg).Result;
+
+                var content = await res.Content.ReadAsStringAsync();
+                var stringContent = Convert.ToString(content);
+                var contentResponse = JsonConvert.DeserializeObject<bool>(stringContent);
+                // Console.WriteLine(content);
+                return contentResponse;
+            }
 
         }
 
 
 
+
+
+
+
+
+
+
+
+        /*  // gettting response as string not as bool form api
+                async Task<string> IsValidCustomer(int custId, int password)
+                {
+                    string url = $"https://localhost:7172/customer/validCustomer/{custId}/{password}";
+                    // make an API request 
+                   // var response = "no";
+                    using (HttpClient client = new HttpClient())
+                    {
+                      // var response = client.GetAsync(url);
+                     //   response.Wait();
+                        //  var temp = JsonConvert.DeserializeObject(response);
+                        var response = await Task.Run(() => client.GetAsync(url).Result);
+                        string temp = await response.Content.ReadAsStringAsync();
+                        // deserilizin the json
+                      //  var customers = JsonConvert.DeserializeObject(response);
+                        if (temp != "no") return "yes";
+
+
+                    }
+                    return "no";
+
+                }
+
+
+        */
 
 
         //bool validLogin(string username, string password)
@@ -227,19 +263,19 @@ namespace LoginPage.ViewModel
         //    Uri uri = new Uri("Page2.xaml", UriKind.Relative);
         //    this.NavigationService.Navigate(uri);
         //}
+
+
+
         private bool CanExecuteLoginCommand(object obj)
         {
             bool canExecute;
-            /*
-            if (Username != null && Password != null)
-           {
+
+            if (Username != 0 && Password != 0)
+            {
                 canExecute = true;
             }
-            */
-            if(IsValidCustomer(Username, Password).Result.ToString() == "yes")
-            {
-                canExecute = true;  
-            }
+
+
             else
             {
                 canExecute = false;
